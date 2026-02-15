0d54de71e7c16605051c85558b81c733697bc308
santeri00 <santeri10.jussila@gmail.com>
2026-02-15 20:50:35 +0200

    make Player model use lombok @Data

--- backend/src/main/java/com/songquiz/backend/model/Player.java
+++ backend/src/main/java/com/songquiz/backend/model/Player.java
@@ -1,5 +1,8 @@
 package com.songquiz.backend.model;
 
+import lombok.Data;
+
+@Data
 public class Player {
   private String sessionId;
   private String nickname;
@@ -13,42 +16,6 @@ public class Player {
     this.score = 0.0;
     this.isHost = false;
     this.guessIsCorrect = false;
-  }
-
-  public String getSessionId() {
-    return sessionId;
-  }
-
-  public void setSessionId(String sessionId) {
-    this.sessionId = sessionId;
-  }
-
-  public String getNickname() {
-    return nickname;
-  }
-
-  public double getScore() {
-    return score;
-  }
-
-  public boolean isHost() {
-    return isHost;
-  }
-
-  public boolean isGuessIsCorrect() {
-    return guessIsCorrect;
-  }
-
-  public void setScore(double score) {
-    this.score = score;
-  }
-
-  public void setHost(boolean isHost) {
-    this.isHost = isHost;
-  }
-
-  public void setGuessIsCorrect(boolean guessIsCorrect) {
-    this.guessIsCorrect = guessIsCorrect;
   }
 
   public void updateScore(double points) {
