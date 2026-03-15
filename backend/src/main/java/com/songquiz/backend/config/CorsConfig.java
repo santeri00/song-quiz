e5d25a16ca4d4050fbe9af3f12835c81a8bf8e84
santeri00 <santeri10.jussila@gmail.com>
2026-03-15 23:02:56 +0200

    fix wrong package

--- backend/src/main/java/com/songquiz/backend/config/CorsConfig.java
+++ backend/src/main/java/com/songquiz/backend/config/CorsConfig.java
@@ -1,6 +1,7 @@
+package com.songquiz.backend.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
